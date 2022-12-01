5e72b33c581c0af2545a2e7081e37217a31cf863
dineshaher <aherdin.laxman@gmail.com>
2022-12-01 09:41:47 +0530

    new commit of working code

--- src/test/java/org/TestRunner/Runner.java
+++ src/test/java/org/TestRunner/Runner.java
@@ -13,6 +13,7 @@ import io.cucumber.junit.Cucumber;
         dryRun=false,
 //		tags="",
         plugin = {"pretty",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" ,
                 "html:target/html_report/cucumber_reports.html",
                 "junit:target/junit_report/junit_reports.xml",
                 "json:target/json_report/json_reports.json",
